44917b4c2d37324077d9cbde71b349d3538e7169
mallouk-bike <spotify.test0707@gmail.com>
2026-01-01 15:01:54 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,7 @@ const URLS_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
@@ -11,15 +12,18 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    Promise.all([
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cache => {
+            if (cache !== CACHE_NAME) {
+              return caches.delete(cache);
+            }
+          })
+        );
+      }),
+      self.clients.claim() // 🔥 PREND LE CONTRÔLE IMMÉDIAT
+    ])
   );
 });
 
